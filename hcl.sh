6b84e25f8694ec975d03cb0efa017fb25dd01492
Hyeonsong Kim <kimhxsong@gmail.com>
2022-03-03 04:25:13 +0900

    Update hcl.sh

--- hcl.sh
+++ hcl.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-BREWDIR="$HOME/goinfre/homebrew"
+export BREWDIR="$HOME/goinfre/homebrew"
 
+rm -rf "$BREWDIR"
+rm -rf "$HOME/goinfre/Applications"
+
+mkdir -p "$HOME/goinfre/Applications"
+ln -s "$HOME/goinfre/Applications" "$HOME/Applications"
 git clone https://github.com/Homebrew/brew $BREWDIR
 
-eval `$BREWDIR/bin/brew shellenv`
-brew update --force --quiet
+# Shell Profile
+$BREWDIR/bin/brew shellenv >> $HOME/.zshrc
+source $HOME/.zshrc
 
-$(brew shellenv) >> $HOME/.zshrc && source $HOME/.zshrc
+# Appdir
+export HOMEBREW_CASK_OPTS="--appdir=$HOME/goinfre/Applications"
 
-brew install gh
+# Formulas
+brew install gh mas numi
+
+# Casks
+mkdir -p "$HOME/goinfre/Library/Application Support/JetBrains"
+ln -s "$HOME/goinfre/Library/Application Support/JetBrains" "/Users/sot/Library/Application Support/JetBrains"
+brew install --cask jetbrains-toolbox
+
+mkdir -p "$HOME/goinfre/Library/Application Support/Notion"
+ln -s "$HOME/goinfre/Library/Application Support/Notion" "/Users/sot/Library/Application Support/Notion"
+brew install --cask Notion
+
+mkdir -p "$HOME/goinfre/Library/Application Support/tabby"
+ln -s "$HOME/goinfre/Library/Application Support/tabby" "/Users/sot/Library/Application Support/tabby"
+brew install --cask tabby
+
+mkdir -p "$HOME/goinfre/Library/Application Support/Google/Chrome Canary"
+ln -s "$HOME/goinfre/Library/Application Support/Google/Chrome Canary" "/Users/sot/Library/Application Support/Google/Chrome Canary"
+brew tap homebrew/cask-versions && brew install --cask google-chrome-canary
+
+# Apps
+mas install 411643860 957734279
 
